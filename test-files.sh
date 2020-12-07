9648035c3a4ebbe853c8639d28af9d69e54b5cbf
anandp-ib <anand.ponnian@infobelt.com>
2020-12-07 06:08:36 +0000

    updated the folder structure

--- test-files.sh
+++ test-files.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 # Create copies of each file type
-dirs=($(find . -not -path "./.git/*" -mindepth 1 -type d))
+#dirs=($(find . -not -path "./.git/*" -mindepth 1 -type d))
+
+dirs=($(find ./unstructured/ -mindepth 1 -type d))
 
 for dir in "${dirs[@]}";
 do
@@ -9,12 +11,12 @@ do
         while read p;
 	do
                 if [ "$p" != "filenames.txt" ]; then
-                        for (( i=0; i<10000; ++i)); do
+                        for (( i=0; i<100; ++i)); do
                                 cp "$p" $i-"$p"
                         done
                 else
                         echo "Working on" "$dir"
                 fi
         done < filenames.txt
-        cd ".."
+        cd "../.."
 done
